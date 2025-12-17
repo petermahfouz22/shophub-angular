c698eadddac31b7a2a00ba3733f19c8c17fa93e9
petermahfouz22 <peter.mahfouz23@gmail.com>
2025-12-17 22:27:11 +0200

    split and organize files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component,signal } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
@@ -8,6 +8,5 @@ import { FooterComponent } from "./footer/footer.component";
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  user = signal(false)
   title = 'e-commerce';
 }
